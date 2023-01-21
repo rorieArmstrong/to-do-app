17a305cb9914b0bce21754897167e8a0ba3558c4
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2023-01-21 14:41:18 +0000

    Updated core beleifs

--- src/pages/CoreBeliefs.jsx
+++ src/pages/CoreBeliefs.jsx
@@ -1,36 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import './CoreBeliefs.css'
 import { useNavigate } from "react-router-dom";
-import { collection, query, onSnapshot, doc, updateDoc, deleteDoc,} from "firebase/firestore";
+import { collection, query, onSnapshot, doc, addDoc, orderBy, where,} from "firebase/firestore";
 import { db } from "../firebase";
 
 
-function CoreBeliefs() {
-    const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
+function CoreBeliefs({user = {uid:''}}) {
     const [combination, setCombination] = useState({ configuration: 6, roundness: 1 })
-
     const navigate = useNavigate();
 
     const navigateTo = () => {
-        setCombination({ configuration: 1, roundness: 1 })
+        setCombination({ configuration: 1, roundness: 4 })
         setTimeout(() => {navigate('/')},750)
         
     }
 
-    const [coreBeliefs, setCoreBeliefs] = useState([]);
+    const [CoreBeliefsArray, setCoreBeliefsArray] = useState([]);
 
     useEffect(() => {
-        const q = query(collection(db, "core_beliefs"));
+        
+    })
+    window.scrollTo({
+        bottom: 0
+    })
+    useEffect(() => {
+        const q = query(collection(db, "CoreBeliefs"), orderBy("created", "desc"), where("uid", "==", user.uid));
         const unsub = onSnapshot(q, (querySnapshot) => {
-        let coreBeliefsArray = [];
+        let CoreBeliefsDB = [];
         querySnapshot.forEach((doc) => {
-            coreBeliefsArray.push({ ...doc.data(), id: doc.id });
+            CoreBeliefsDB.push({ ...doc.data(), id: doc.id });
         });
-        setCoreBeliefs(coreBeliefsArray);
+        setCoreBeliefsArray(CoreBeliefsDB);
         });
         return () => unsub();
     }, []);
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (body !== "" && title !== "") {
+        await addDoc(collection(db, "CoreBeliefs"), {
+            title: title,
+            body: body,
+            created: Date.now(),
+            uid: user.uid,
+        });
+            setBody("");
+            setTitle("");
+        }
+    };
 
     const handleEdit = async (coreBeliefs, title) => {
         await updateDoc(doc(db, "todos", coreBeliefs.id), { title: title });
@@ -40,20 +60,51 @@ function CoreBeliefs() {
     };
 
     return ( <div id='wrapper' data-configuration={combination.configuration} data-roundness={combination.roundness} >
-        <div  onClick={() => navigateTo()} className='shape'>
+        <div  className='shape'>
         </div>
-        <div  onClick={() => navigateTo()} className='shape'>
+        <div  className='shape'>
         </div>
-        <div  onClick={() => navigateTo()} className='shape'>
-            <h2>Core beliefs</h2>
+        <div  className='shape'>
+            <div>
+                <h2 onClick={() => navigateTo()}>Core Beliefs</h2>
+            </div>
+            <div className='content' id='CoreBeliefs-Content'>
+                <form onSubmit={handleSubmit}>
+                    <div className="input_container">
+                        <input 
+                            placeholder="Title"
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            id='CoreBeliefs'
+                        />
+                        <textarea
+                            placeholder="Details ..."
+                            value={body}
+                            onChange={(e) => setBody(e.target.value)}
+                            id='CoreBeliefs'
+                        />
+                    </div>
+                    <div className="btn_container">
+                        <button id='CoreBeliefs'>Add</button>
+                    </div>
+                </form>
+                {CoreBeliefsArray.map(({id, title , body}) => {
+                    return(
+                        <div className='Content' id='CoreBeliefs' key={id}>
+                            <h4>{title}</h4>
+                            <p>{body}</p>
+                        </div>)
+                }) }
+            </div>
         </div>
-        <div  onClick={() => navigateTo()} className='shape'>
+        <div  className='shape'>
         </div>
-        <div  onClick={() => navigateTo()} className='shape'>
+        <div  className='shape'>
         </div>
-        <div  onClick={() => navigateTo()} className='shape'>
+        <div   className='shape'>
         </div>
-        <div  onClick={() => navigateTo()} className='shape'>
+        <div  className='shape'>
         </div>
     </div> );
 }
